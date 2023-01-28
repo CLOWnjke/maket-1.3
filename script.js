e1ec71e68a4328ac459d379e3ac80befd1517427
CLOWnjke <dnqbezdar@gmail.com>
2023-01-28 19:24:46 +0500

    28.01

--- script.js
+++ script.js
@@ -1,42 +1,63 @@
 const slider = document.querySelector('.btn-slider');
 
-let mySwiper;
+new Swiper(slider, {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+            
+        slidesPerView: 1.25,
 
-function mobileSlider() {
-    if (window.innerWidth <= 768 && slider.dataset.mobile == 'false') {
-        mySwiper = new Swiper(slider, {
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true
+        breakpoints: {
+            768: {
+              enabled: true,
+              slidesPerView: 1,
+              speed: 800,
             },
-        
-            slidesPerView: 1.25,
-        });
-
-        slider.dataset.mobile = 'true';
-    }
-
-    if (window.innerWidth > 768) {
-        slider.dataset.mobile = 'false';
-
-        mySwiper.destroy();
-    }
-}
-
-mobileSlider();
-
-window.addEventListener('resize', () => {
-    mobileSlider();
+          },
 });
+
+
+
+// const slider = document.querySelector('.btn-slider');
+
+// let mySwiper;
+
+// function mobileSlider() {
+//     if (window.innerWidth <= 768 && slider.dataset.mobile == 'false') {
+//         mySwiper = new Swiper(slider, {
+//             pagination: {
+//                 el: '.swiper-pagination',
+//                 clickable: true
+//             },
+        
+//             slidesPerView: 1.25,
+//         });
+
+//         slider.dataset.mobile = 'true';
+//     }
+
+//     if (window.innerWidth > 768) {
+//         slider.dataset.mobile = 'false';
+
+//     }
+// }
+
+// mobileSlider();
+
+// window.addEventListener('resize', () => {
+//     mobileSlider();
+// });
 
 
 let button = document.querySelector('.main__btn');
 let slide = document.querySelectorAll('.disable');
-button.textContent = 'Показать все';
+let highlight = document.querySelector('.main--highlight');
 
 button.addEventListener('click', function () {
     for (let i = 0; i < slide.length; i++) {
         slide[i].classList.toggle('disable');
     }
     button.textContent = (button.textContent === 'Скрыть') ? 'Показать все' : 'Скрыть';
+    highlight.classList.toggle('main--highlight--active');
 });
